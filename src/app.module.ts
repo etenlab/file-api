ba03532d86fc1e8fd7dd89b5e52e0759a0c2f4fa
hiroshi <hiroshi.tan57@gmail.com>
2022-11-07 16:59:53 +0000

    config typeorm and add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,17 @@ dotenv.config();
         'graphql-ws': true,
       },
     }),
+    TypeOrmModule.forRoot({
+      keepConnectionAlive: true,
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
   ],
 })
 export class AppModule {}
